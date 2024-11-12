bce1943e45f09a9c8c0060716eba78e185e9a844
shahriarnaiem-rafi <shahriarnaiemrafi33@gmail.com>
2024-11-12 10:02:42 +0600

    initial

---
+++ New folder/var_statement_typeof.php
@@ -0,0 +1,14 @@
+<?php
+
+    // echo "Hello world";
+    // print "my world";
+    // print_r ("hello world");
+    // printf('hello0');
+
+    $a="45 sdfg";
+    $b=43;
+    $c=$a+$b;
+    var_dump($a);#type of check
+    echo var_dump($c); 
+
+?>
